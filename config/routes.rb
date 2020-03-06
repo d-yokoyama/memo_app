db12faa3cc3c4d80c8f6de16e8ee43681db1a8f0
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-06 14:07:27 +0900

    maked post create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get "posts/index" => "posts#index"
+  post "posts/create" => "posts#create"
+
+  get "/" => "home#top"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_05_014823) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
