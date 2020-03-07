c83293883619971b6fcb4d53d71ba550e2444456
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-08 07:59:58 +0900

    add home_view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def top
-    
+  end
+  def top_view
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "home#top_view"
+  get "/favicon.ico" => "heme#top_view"
+
   get "posts/index" => "posts#index"
   post "posts/create" => "posts#create"
   get "posts/:id/show" => "posts#show"
