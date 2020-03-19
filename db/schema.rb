330437f8d0ec1fd71159bc4adf98cc2906155f50
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-19 09:51:46 +0900

    add user_id_column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_233355) do
+ActiveRecord::Schema.define(version: 2020_03_19_003024) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
