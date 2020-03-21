78c27159bb4c4f88f4fb95d5db9e40df1aa729ac
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-21 16:57:33 +0900

    Add guest_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_003024) do
+ActiveRecord::Schema.define(version: 2020_03_21_074341) do
+
+  create_table "guests", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
