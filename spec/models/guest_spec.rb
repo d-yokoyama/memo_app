08980c6cb974e8e459822eebe352f8213003ea86
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-21 22:24:18 +0900

    Add guest_rspec

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "guestログインしたら初期値を作成する"  do
+    guest = Guest.new(content: "ゲストログインです")
+    expect(guest.content).to eq "ゲストログインです"
+  end
 end
