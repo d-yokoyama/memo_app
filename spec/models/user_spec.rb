637ef12b6b7e584c2d88579c48f58e310b7d24e8
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-27 12:15:20 +0900

    Gem_FactorBotの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe User, type: :model do
 
   it "ユーザーID,password,emailがあれば登録できる"
 
+  it "ユーザーIDがなければ登録できない"
+
+  it "passwordがなければ登録できない"
+
+  it "emailがなければ登録できない"
+
+  it "ユーザーIDが重複してたら登録できない"
+
+  it "emailが重複してたら登録できない"
+
+  it "passwordが暗号化されているか"
+
 end
