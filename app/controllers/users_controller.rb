445ef55b773ad7fcaafb462824fedbbdc8380683
d-yokoyama <d88.yokoyama@gmail.com>
2020-03-27 19:45:25 +0900

    新規登録時に同じIDとメールアドレスが登録されている場合は登録できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
   def signup
-    flash[:notice] = "新規登録しました"
-    session[:user_id] = params[:id]
-    session[:name] = params[:name]
-    @user = User.new(
-      name: params[:name],
-      email: params[:email],
-      password: params[:password]
-    )
-    @user.save
-    redirect_to("/posts/create")
+    if User.find_by(name: params[:name])
+      flash[:notice] = "このIDはすでに使われています"
+      redirect_to("/")
+    elsif User.find_by(email: params[:email])
+      flash[:notice] = "このメールアドレスは既に登録されています"
+      redirect_to("/")
+    else
+      flash[:notice] = "新規登録しました"
+      session[:user_id] = params[:id]
+      session[:name] = params[:name]
+      @user = User.new(
+        name: params[:name],
+        email: params[:email],
+        password: params[:password]
+      )
+      @user.save
+      redirect_to("/posts/create")
+    end
   end
   def signin
     @user = User.find_by(
